663f37bd6e749031d4cd6d4e511edcbd854cd894
mgrbo <mgrbo@web.de>
2021-09-19 17:43:14 +0200

    version bump, fixed mod.json vehicle

--- BTSimpleMechAssembly/SimpleMechAssembly_Init.cs
+++ BTSimpleMechAssembly/SimpleMechAssembly_Init.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-[assembly: AssemblyVersion("1.1.3.2")]
+[assembly: AssemblyVersion("1.2.0.0")]
 
 namespace BTSimpleMechAssembly
 {
